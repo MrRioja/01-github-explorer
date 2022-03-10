09bc3c336eb2d1b61d4038c9c00db1a7dfdb5552
Luiz Rioja <lulyrioja@gmail.com>
2022-03-09 23:59:33 -0300

    chore(App): renders first created component and uses global styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+import "./styles/global.scss";
+import { RepositoryList } from "./components/RepositoryList";
+
 export function App() {
-  return <h1>Hello world</h1>;
+  return <RepositoryList />;
 }
