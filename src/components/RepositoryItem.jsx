a46072d046def803ad1e3024c7101f19c5d40d15
Luiz Rioja <lulyrioja@gmail.com>
2022-03-10 23:45:18 -0300

    chore(repositoryItem): create component to be displayed in the repositories list

---
+++ src/components/RepositoryItem.jsx
@@ -0,0 +1,9 @@
+export function RepositoryItem() {
+  return (
+    <li>
+      <strong>unform</strong>
+      <p>Forms in React</p>
+      <a href="">Acessar repositório</a>
+    </li>
+  );
+}
--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,26 +1,15 @@
+import { RepositoryItem } from "./RepositoryItem";
+
 export function RepositoryList() {
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
 
       <ul>
-        <li>
-          <strong>unform</strong>
-          <p>Forms in React</p>
-          <a href="">Acessar repositório</a>
-        </li>
-
-        <li>
-          <strong>unform</strong>
-          <p>Forms in React</p>
-          <a href="">Acessar repositório</a>
-        </li>
-
-        <li>
-          <strong>unform</strong>
-          <p>Forms in React</p>
-          <a href="">Acessar repositório</a>
-        </li>
+        <RepositoryItem />
+        <RepositoryItem />
+        <RepositoryItem />
+        <RepositoryItem />
       </ul>
     </section>
   );
