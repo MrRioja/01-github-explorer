80b0adca005409f66e54a61c79c7d844f60bba68
Luiz Rioja <lulyrioja@gmail.com>
2022-03-12 00:38:50 -0300

    feat(repositoryList): fetch data from repositories on github and render on screen

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,16 +1,24 @@
+import { useState, useEffect } from "react";
 import "../styles/repositories.scss";
 import { RepositoryItem } from "./RepositoryItem";
 
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.github.com/users/mrrioja/repos")
+      .then((response) => response.json())
+      .then((data) => setRepositories(data));
+  }, []);
+
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
 
       <ul>
-        <RepositoryItem />
-        <RepositoryItem />
-        <RepositoryItem />
-        <RepositoryItem />
+        {repositories.map((repository) => (
+          <RepositoryItem key={repository.name} repository={repository} />
+        ))}
       </ul>
     </section>
   );
