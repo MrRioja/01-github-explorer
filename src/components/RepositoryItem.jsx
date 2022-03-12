46928177f557eef9dca2a042a2799c404ceff95c
Luiz Rioja <lulyrioja@gmail.com>
2022-03-12 15:36:57 -0300

    refac: converts js files to ts

--- src/components/RepositoryItem.jsx
+++
@@ -1,9 +0,0 @@
-export function RepositoryItem(props) {
-  return (
-    <li>
-      <strong>{props.repository.name}</strong>
-      <p>{props.repository.description}</p>
-      <a href={props.repository.html_url}>Acessar repositório</a>
-    </li>
-  );
-}
--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -1,9 +1,15 @@
-import { useState, useEffect } from "react";
 import "../styles/repositories.scss";
+import { useState, useEffect } from "react";
 import { RepositoryItem } from "./RepositoryItem";
 
+interface Repository {
+  name: string;
+  description: string;
+  html_url: string;
+}
+
 export function RepositoryList() {
-  const [repositories, setRepositories] = useState([]);
+  const [repositories, setRepositories] = useState<Repository[]>([]);
 
   useEffect(() => {
     fetch("https://api.github.com/users/mrrioja/repos")
